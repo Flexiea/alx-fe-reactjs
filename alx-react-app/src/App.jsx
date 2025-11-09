4341cf336bfdc77a54294158f578a57c4be6a7c1
Kayode Adebisi <Flexieamiles@gmail.com>
2025-11-09 22:47:50 +0100

    Added component folder

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -9,6 +10,16 @@ function App() {
       <Header />
       <MainContent />
       <WelcomeMessage />
+      <WelcomeMessage />
+
+<UserProfile 
+  name="Alice" 
+  age={25} 
+  bio="Loves hiking and photography" 
+/>
+
+<Footer />
+
       <Footer />
     </div>
   );

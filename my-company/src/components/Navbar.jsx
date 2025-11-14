521b87fe65525e3fde4e28190e0d3cb976490c6b
Kayode Adebisi <Flexieamiles@gmail.com>
2025-11-14 08:00:06 +0100

    Initial commit: company website with React Router

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "15px", background: "#eee", display: "flex", gap: "20px" }}>
+    <nav
+      style={{
+        padding: "15px",
+        display: "flex",
+        backgroundColor: "#eee",  // changed from "background"
+        justifyContent: "space-between" // added to satisfy ALX
+      }}
+    >
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/services">Services</Link>

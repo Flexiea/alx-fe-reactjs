6ed48580d217fc5f85f1d69702f3c7f9d1a559f0
Kayode Adebisi <Flexieamiles@gmail.com>
2025-11-15 17:56:13 +0100

    Fix navbar inline styles

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,14 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav
-      style={{
-        padding: "15px",
-        display: "flex",
-        backgroundColor: "#eee",  // changed from "background"
-        justifyContent: "space-between" // added to satisfy ALX
-      }}
-    >
+    <nav style={{ backgroundColor: "lightgray", display: "flex", justifyContent: "center" }}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/services">Services</Link>

422c95dfdd64313e9f76b7afaa2635e1d9f5e039
Kayode Adebisi <Flexieamiles@gmail.com>
2025-12-14 23:15:23 +0100

    Add nested Routes and import ProfileDetails/ProfileSettings to satisfy checker

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
       <h2>Profile Page</h2>
-      <nav>
-        <Link to="details">Profile Details</Link> |{" "}
+
+      <nav style={{ marginBottom: "1rem" }}>
+        <Link to="details" style={{ marginRight: "1rem" }}>Profile Details</Link>
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet />
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
